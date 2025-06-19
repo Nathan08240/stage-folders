498682662b0d8afd4ea41a0607275d90e56cddd9
Nathan BRANCOS <branconathan@gmail.com>
2025-06-19 10:58:17 +0200

    add user routes

--- api-rest/main.js
+++ api-rest/main.js
@@ -1,6 +1,7 @@
 import e from "express";
 import { connection } from "./database.js";
 import vehiclesRouter from "./routes/vehicles.js";
+import usersRouter from "./routes/users.js";
 
 const app = e();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 app.use(e.json());
 
 app.use("/", vehiclesRouter);
+app.use("/", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ api-rest/routes/users.js
@@ -0,0 +1,4 @@
+import { connection } from "../database.js";
+import { Router } from "express";
+
+const router = Router();
